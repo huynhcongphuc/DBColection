8b61a8c8bb1ffdd960276735d03fdec8abc5874f
unknown <phuchuynh247@gmail.com.vn>
2021-09-23 11:19:59 +0700

    test

---
+++ APIDB.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mysql = require('mysql');
+var sql = require('mssql');
+const app = express();
+//MSSQL server
+
+// var dbConfig = {
+//   server: "localhost",
+//   database: "FLISR_LOG",
+//   user: "phuchc",
+//   password: "123456",
+//   trustServerCertificate: true,
+//   port: 1433
+// };
+
+// var conn = new sql.ConnectionPool(dbConfig);
+
+// conn.connect(function (err) {
+//   (err) ? console.log(err) : console.log(conn);
+// });
+
+
+// app.post('/api/mssql', (req, res) => {
+//   var sqlqr = "SELECT * From DataList";
+//   conn.query(sqlqr, function (err, recordset) {
+//     if (err) throw err;
+//     res.json({ recordset });
+//   });
+// });
+
+//Mongo connection
+// var MongoClient = require('mongodb').MongoClient;
+// var db;
+
+// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+//   // Client returned
+//   db = client.db('Data');
+// });
+
+// app.post('/api/mongo', (req, res) => {
+//   db.collection('Account').find().toArray(function (err, docs) {
+//     if (err) throw err;
+//     res.json({ news: docs });
+//   });
+// });
+
+// //Mysql Connection
+const connection = mysql.createConnection({
+  host: 'sql6.freemysqlhosting.net',
+  user: 'sql6438575',
+  password: 'IDuV5V7kJJ',
+  database: 'sql6438575'
+});
+
+connection.connect(function (err) {
+  (err) ? console.log(err) : console.log(connection);
+});
+
+app.post('/api/news', (req, res) => {
+  var sql = "SELECT * FROM Data";
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
+    res.json({ news: results });
+  });
+});
+
+//Get
+app.get('/', (req, res) => {
+  res.send('Xin chao');
+});
+
+app.listen(4000, () => console.log('Server listening on port 4000!') );
